3b6a433f200c363d357217aff211d1ccd7fcc1c1
oklekot <oklekot@griddynamics.com>
2024-03-12 16:53:40 +0100

    Adding GameValidator and InputValidator tests.

--- src/test/java/com/griddynamics/tictactoe/validator/GameValidatorTest.java
+++ src/test/java/com/griddynamics/tictactoe/validator/GameValidatorTest.java
@@ -1,21 +1,33 @@
 package com.griddynamics.tictactoe.validator;
 
+import com.griddynamics.tictactoe.board.Board;
+import com.griddynamics.tictactoe.game.GameConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class GameValidatorTest {
+
+    private Board board;
 
     @BeforeEach
     void setUp() {
-
+        Board.SIZE = 3;
+        board = new Board();
     }
 
     @Test
     void validateCellAvailability() {
+            assertTrue(GameValidator.validateCellAvailability(1, 1, this.board));
+            board.setCellStatus(1, 1, GameConstants.X_SIGN);
+            assertFalse(GameValidator.validateCellAvailability(1, 1, board));
       }
 
     @Test
     void validateCoordinatesRange() {
+            assertTrue(GameValidator.validateCoordinatesRange(2, 2));
+            assertFalse(GameValidator.validateCoordinatesRange(222222, 2341));
       }
 }
